85db90c3c710c0c477ae1dd86b2f39f805a9207f
Snehashis <mandalsnehashis35@gmail.com>
2026-03-14 12:15:52 +0530

    first commit

---
+++ src/test/java/code/GoogleTitleTest.java
@@ -0,0 +1,27 @@
+package code;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class GoogleTitleTest {
+
+	@Test
+    public void LoginTest() {
+
+        // Launch Chrome Browser
+        WebDriver driver = new ChromeDriver();
+
+        // Open Google
+        driver.get("https://www.google.com");
+
+        // Get page title
+        String title = driver.getTitle();
+
+        // Print title
+        System.out.println("Page Title is: " + title);
+
+        // Close browser
+        driver.quit();
+    }
+}
